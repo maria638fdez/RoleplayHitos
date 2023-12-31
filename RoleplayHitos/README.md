f48e164e3115d7cadfbaa18b0ae37ee6131d08de
maria638fdez <mariaf@uma.es>
2023-12-31 12:51:34 +0100

    Files

--- RoleplayHitos/README.md
+++
@@ -1,2 +0,0 @@
-# RoleplayHitos
-An app that manages a roleplay table for an Hitos campaign.
--- RoleplayHitos/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module roleplayHitos {
-	requires javafx.controls;
-	requires transitive javafx.graphics;
-	requires java.desktop;
-	exports app;
-}
